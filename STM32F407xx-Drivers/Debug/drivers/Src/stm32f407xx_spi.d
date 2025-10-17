fd7c4542a52f1691ca6ad701c5e8cd22b510087d
Omer Faruk Yesir <omeryesir@gmail.com>
2025-10-17 20:12:38 +0300

    Add all project files including debug outputs

--- STM32F407xx-Drivers/Debug/drivers/Src/stm32f407xx_spi.d
+++ STM32F407xx-Drivers/Debug/drivers/Src/stm32f407xx_spi.d
@@ -1,9 +1,9 @@
 drivers/Src/stm32f407xx_spi.o: ../drivers/Src/stm32f407xx_spi.c \
- C:/Users/ÖMER\ FARUK\ YESİR/Desktop/STM32F407xx-Drivers/drivers/Inc/stm32f407xx_spi.h \
- C:/Users/ÖMER\ FARUK\ YESİR/Desktop/STM32F407xx-Drivers/drivers/Inc/stm32f407xx.h \
- C:/Users/ÖMER\ FARUK\ YESİR/Desktop/STM32F407xx-Drivers/drivers/Inc/stm32f407xx_gpio.h \
- C:/Users/ÖMER\ FARUK\ YESİR/Desktop/STM32F407xx-Drivers/drivers/Inc/stm32f407xx_spi.h
-C:/Users/ÖMER\ FARUK\ YESİR/Desktop/STM32F407xx-Drivers/drivers/Inc/stm32f407xx_spi.h:
-C:/Users/ÖMER\ FARUK\ YESİR/Desktop/STM32F407xx-Drivers/drivers/Inc/stm32f407xx.h:
-C:/Users/ÖMER\ FARUK\ YESİR/Desktop/STM32F407xx-Drivers/drivers/Inc/stm32f407xx_gpio.h:
-C:/Users/ÖMER\ FARUK\ YESİR/Desktop/STM32F407xx-Drivers/drivers/Inc/stm32f407xx_spi.h:
+ C:/Users/Asus/Desktop/STM32F407xx-Drivers/STM32F407xx_Baremetal_Driver_Dev/STM32F407xx-Drivers/drivers/Inc/stm32f407xx_spi.h \
+ C:/Users/Asus/Desktop/STM32F407xx-Drivers/STM32F407xx_Baremetal_Driver_Dev/STM32F407xx-Drivers/drivers/Inc/stm32f407xx.h \
+ C:/Users/Asus/Desktop/STM32F407xx-Drivers/STM32F407xx_Baremetal_Driver_Dev/STM32F407xx-Drivers/drivers/Inc/stm32f407xx_gpio.h \
+ C:/Users/Asus/Desktop/STM32F407xx-Drivers/STM32F407xx_Baremetal_Driver_Dev/STM32F407xx-Drivers/drivers/Inc/stm32f407xx_spi.h
+C:/Users/Asus/Desktop/STM32F407xx-Drivers/STM32F407xx_Baremetal_Driver_Dev/STM32F407xx-Drivers/drivers/Inc/stm32f407xx_spi.h:
+C:/Users/Asus/Desktop/STM32F407xx-Drivers/STM32F407xx_Baremetal_Driver_Dev/STM32F407xx-Drivers/drivers/Inc/stm32f407xx.h:
+C:/Users/Asus/Desktop/STM32F407xx-Drivers/STM32F407xx_Baremetal_Driver_Dev/STM32F407xx-Drivers/drivers/Inc/stm32f407xx_gpio.h:
+C:/Users/Asus/Desktop/STM32F407xx-Drivers/STM32F407xx_Baremetal_Driver_Dev/STM32F407xx-Drivers/drivers/Inc/stm32f407xx_spi.h:
--- STM32F407xx-Drivers/Debug/drivers/Src/subdir.mk
+++ STM32F407xx-Drivers/Debug/drivers/Src/subdir.mk
@@ -1,6 +1,6 @@
 ################################################################################
 # Automatically-generated file. Do not edit!
-# Toolchain: GNU Tools for STM32 (13.3.rel1)
+# Toolchain: GNU Tools for STM32 (11.3.rel1)
 ################################################################################
 
 # Add inputs and outputs from these tool invocations to the build variables 
@@ -19,7 +19,7 @@ C_DEPS += \
 
 # Each subdirectory must supply rules for building sources it contributes
 drivers/Src/%.o drivers/Src/%.su drivers/Src/%.cyclo: ../drivers/Src/%.c drivers/Src/subdir.mk
-	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DSTM32 -DSTM32F4 -DSTM32F407VGTx -c -I../Inc -I"C:/Users/ÖMER FARUK YESİR/Desktop/STM32F407xx-Drivers/drivers/Inc" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DSTM32 -DSTM32F4 -DSTM32F407VGTx -c -I../Inc -I"C:/Users/Asus/Desktop/STM32F407xx-Drivers/STM32F407xx_Baremetal_Driver_Dev/STM32F407xx-Drivers/drivers/Inc" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
 
 clean: clean-drivers-2f-Src
 
